e9d1ece6b988f9b9a98608ce208fa0bebe428709
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-11 10:08:00 +0330

    Add files via upload

---
+++ WindowsFormsApp70/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp70
+{
+    class cValid
+    {
+      public int lenght
+        {
+            get;
+            set;
+        }
+
+        private string ct;
+        public string Text
+        {
+            get
+            {
+                return ct;
+            }
+            set
+            {
+                if (value.Length <= lenght)
+                {
+                    ct = value;
+                    for (int i = 0; i < ct.Length; i++)
+                    {
+                        if (!char.IsLetter(char.Parse(ct.Substring(i, 1))))
+                            ct = ct.Remove(i, 1);
+                    }
+                }
+            }
+        }
+
+    }
+}
